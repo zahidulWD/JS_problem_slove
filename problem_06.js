5bac88dc9c97852549086c975b1cf2d0e45c1daf
Zahidul Islam <zahidul670791@gmail.com>
2025-10-05 21:53:33 -0700

    problem_06 slove

---
+++ problem_06.js
@@ -0,0 +1,20 @@
+
+// problem
+// Problem 6: Check Voting Eligibility (use ternary operator)
+// Write a program to check whether a person is eligible to vote or not.
+// Hint: A person is eligible if age >= 18.
+// Sample Input/Output:
+
+// Input: 16 → Output: Not Eligible
+// Input: 20 → Output: Eligible
+
+
+// solove
+let age = 20;
+
+if(age >= 18){
+    console.log("Eligible");
+}
+else{
+    console.log("Not Eligible");
+}
