bb9eb4d6ede4e8535329d0d84ee7536d5a7aabe7
Zahidul Islam <zahidul670791@gmail.com>
2025-10-05 22:37:23 -0700

    problem_10 slove

---
+++ problem_10.js
@@ -0,0 +1,23 @@
+
+
+// problem_10
+
+// Problem 10: Check Vowel or Consonant
+// Write a program to check if a given alphabet is a vowel or a consonant.
+// Hint: Vowels are a, e, i, o, u.
+// Sample Input/Output:
+
+// Input: a → Output: Vowel
+// Input: b → Output: Consonant
+
+
+
+// slove
+
+let latter = "b";
+
+if(latter == "a" || latter == "e" || latter == "i" || latter == "o" || latter == "u"){
+    console.log(latter, "is Vowel" );
+}else{
+    console.log(latter, "is Consonant");
+}
