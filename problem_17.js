d6b86af199efe454322e548d06aae8844f25c7fe
Zahidul Islam <zahidul670791@gmail.com>
2025-10-06 02:34:24 -0700

    problem_17 slove

---
+++ problem_17.js
@@ -0,0 +1,24 @@
+
+
+// problem_17
+// Write a program to check if a student has passed or failed.
+// Hint: Pass if marks >= 40.
+// Sample Input/Output:
+
+// Input: 35 → Output: Fail
+// Input: 60 → Output: Pass
+
+
+
+// slove
+
+let mark = 35;
+
+if(mark >= 40){
+    console.log("Pass");
+}else{
+    console.log("Fail");
+}
+
+
+// output : Fail
