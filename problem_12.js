3fcb393f64c6c00c138037bde1ded74f5eddec56
Zahidul Islam <zahidul670791@gmail.com>
2025-10-05 23:06:40 -0700

    problem_12 slove

---
+++ problem_12.js
@@ -0,0 +1,32 @@
+
+
+// problem_12
+  
+// Problem 12: Simple Calculator (+, -, *, /)
+// Write a program to perform +, -, *, / between two numbers.
+// Hint: Use if...else if to check operator.
+// Sample Input/Output:
+
+// Input: 10 5 + → Output: 15
+// Input: 10 5 * → Output: 50
+
+
+
+// slove
+
+let a = 10;
+let b = 5;
+
+let oparator= "*";
+
+if(oparator === "+"){
+    console.log(a+b);
+}else if(oparator === "-"){
+    console.log(a-b);
+}else if(oparator === "*"){
+    console.log(a*b);
+}else if(oparator === "/"){
+    console.log(a/b);
+}else{
+    console.log("invalid oparator");
+}
