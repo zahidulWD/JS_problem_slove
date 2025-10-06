f11063951adff29dd0626f764f6e59d0773af691
Zahidul Islam <zahidul670791@gmail.com>
2025-10-05 20:37:18 -0700

    Problem_01 slove

---
+++ problem_01.js
@@ -0,0 +1,18 @@
+
+// problem_01
+
+// Write a program to check whether a given number is even or odd.
+// Hint: Use modulus operator %.
+// Sample Input/Output:
+
+// Input: 7 → Output: Odd
+// Input: 10 → Output: Even
+
+
+let number = 10 ;
+
+if(number % 2 === 0){
+    console.log(number, "is even");
+}else{
+    console.log(number,"is odd");
+}
