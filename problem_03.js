393b67b6dee989d2e0d2a41e82b87994cab47b6d
Zahidul Islam <zahidul670791@gmail.com>
2025-10-05 21:14:12 -0700

    problem_03 slove

---
+++ problem_03.js
@@ -0,0 +1,15 @@
+
+// problem_03
+// Problem 3: Find Maximum of Two Numbers (use ternary operator)
+// Write a program to find the maximum number between two given numbers.
+// Hint: Use ternary condition ? value1 : value2.
+// Sample Input/Output:
+
+// Input: 10 20 → Output: 20
+
+
+let num1 = 10;
+let num2 = 50;
+
+let maximum = (num1 > num2) ? num1: num2;
+console.log("maximum number:",maximum);
